4c22ea7f6463eb0a3e93ce71563224716b1da015
LaTaevia Berry <lataevia.berry@gmail.com>
2016-05-01 22:46:27 -0700

    add random tangent

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,16 +15,27 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/word", (request, response) -> {
-          Map<String, Object> model = new HashMap<String, Object>();
+    get("/word", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String newVocabWord = request.queryParams("vocabWord");
+      Word newWord = new Word(newVocabWord);
+      String test = newWord.getVocabWord();
+      model.put("test", test);
+      request.session().attribute("test");
+      model.put("template", "templates/fullWords.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
-          String vocabWord = request.queryParams("vocabWord");
-          Word newWord = new Word(vocabWord);
-          request.session().attribute("vocabWord", newWord);
-
-          model.put("template", "templates/define.vtl");
-          return new ModelAndView(model, layout);
-        }, new VelocityTemplateEngine());
+    // get("/word", (request, response) -> {
+    //   Map<String, Object> model = new HashMap<String, Object>();
+    //
+    //   String vocabWord = request.queryParams("vocabWord");
+    //   Word newWord = new Word(vocabWord);
+    //   request.session().attribute("vocabWord", newWord);
+    //
+    //   model.put("template", "templates/define.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
 
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,6 +3,8 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,11 +25,33 @@ public class AppTest extends FluentTest {
     assertThat(pageSource()).contains("Construct Your Own Dictionary!");
   }
 
+  // @Test
+  // public void wordIsCreatedTest() {
+  //   goTo("http://localhost:4567/");
+  //   fill("#vocabWord").with("SavedWord");
+  //   submit(".btn");
+  //   assertThat(pageSource()).contains("Your word has been saved.");
+  // }
+
   @Test
-  public void wordIsCreatedTest() {
+  public void WordIsDisplayedTest() {
     goTo("http://localhost:4567/");
     fill("#vocabWord").with("SavedWord");
     submit(".btn");
-    assertThat(pageSource()).contains("Your word has been saved.");
+    click("a", withText("Go Back"));
+    assertThat(pageSource()).contains("SavedWord");
   }
+
+  // @Test
+  // public void multipleWordsAreDisplayedTest() {
+  //   goTo("http://localhost:4567/");
+  //   fill("#vocabWord").with("Multi Words");
+  //   submit(".btn");
+  //   click("a", withText("Go Back"));
+  //   fill("#vocabWord").with("Words Multi");
+  //   submit(".btn");
+  //   click("a", withText("Go Back"));
+  //   assertThat(pageSource()).contains("Multi Words");
+  //   assertThat(pageSource()).contains("Words Multi");
+  // }
 }
